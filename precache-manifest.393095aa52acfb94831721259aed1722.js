a49d6af3a0ab1082027c7c937a4a1ea67e1e0507
byseop <byseop@gmail.com>
2019-04-23 19:04:15 +0900

    Updates

---
+++ precache-manifest.393095aa52acfb94831721259aed1722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  },
+  {
+    "revision": "d6b3c0f236e685d9efd5",
+    "url": "/movie-app/static/js/main.8b80e708.chunk.js"
+  },
+  {
+    "revision": "f2d68859eabeb2ed8c4a",
+    "url": "/movie-app/static/js/2.cd61bd99.chunk.js"
+  },
+  {
+    "revision": "d6b3c0f236e685d9efd5",
+    "url": "/movie-app/static/css/main.c00f4abb.chunk.css"
+  },
+  {
+    "revision": "019c368d4b4c49c56465a141837d2bf6",
+    "url": "/movie-app/index.html"
+  }
+];
